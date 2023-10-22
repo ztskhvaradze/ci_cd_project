48c7dc4f5687f80d8911a58b51d635bf6745a88f
Zaur Tskhvaradze <93708582+ztskhvaradze@users.noreply.github.com>
2023-10-22 11:17:17 +0400

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -2,7 +2,7 @@ import pytest
 import pymssql
 
 # Server and user information
-MSSQL_SERVER = "EPGETBIW052E"
+MSSQL_SERVER = "EPGETBIW052E,1433"
 MSSQL_USER = "test_user"
 MSSQL_PASSWORD = "test_password"
 MSSQL_DATABASE = "AdventureWorks2012"
